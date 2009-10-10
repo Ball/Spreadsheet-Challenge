6e12fbebed1b2b0e81b1676ce131551468ac5a28
unknown <bjball@.net.sep.com>
2009-10-10 10:58:29 -0400

    Cleaned up unit tests

--- TestCases/Challenge1.fs
+++ TestCases/Challenge1.fs
@@ -4,43 +4,47 @@ open FsxUnit.Syntax
 open Xunit
 open Module1
 
+let should_become expected insert =
+  match insert with
+  | (cell, value) -> put_cell cell value new_sheet |> get_cell cell |> should equal expected
+  | _ -> false |> should be True
+let should_be_empty cell =
+  get_cell cell new_sheet |> should equal ""
+let should_store_in_sequence cell_additions =
+  let rec loop cell_additions sheet =
+    match cell_additions with
+    | (cell, value) :: t ->
+        let current_sheet =  put_cell cell value sheet
+        current_sheet |> get_cell cell |> should equal value
+        loop t current_sheet
+    | [] -> true |> should be True
+  loop cell_additions new_sheet
+
 [<Fact>]
 let cells_be_empty_on_default() =
-  let sheet = new_sheet
-  sheet |> get_cell "A1" |> should equal ""
-  sheet |> get_cell "ZX347" |> should equal ""
+  "A1" |> should_be_empty
+  "ZX347" |> should_be_empty
 
 [<Fact>]
 let cells_should_store_data() =
-  let sheet = new_sheet
-  let next = sheet |> put_cell "A21" "A string"
-  next |> get_cell "A21" |> should equal "A string"
-  let another = next |> put_cell "A21" "A different thing"
-  another |> get_cell "A21" |> should equal "A different thing"
+  [("A21", "A string"); ("A21", "A different thing")]
+  |> should_store_in_sequence
 
 [<Fact>]
 let there_should_be_many_cells() =
-  let sheet = new_sheet
-  let stored = (sheet
-    |> put_cell "A21" "First"
-    |> put_cell "X27" "Second"
-    |> put_cell "ZX901" "Third")
-  stored |> get_cell "A21" |> should equal "First"
-  stored |> get_cell "X27" |> should equal "Second"
-  stored |> get_cell "ZX901" |> should equal "Third"
+  [("A21", "First"); ("X27", "Second"); ("ZX901", "Third")]
+  |> should_store_in_sequence
 
 [<Fact>]
 let it_should_format_strings_like_strings() =
-  let sheet = new_sheet |> put_cell "A1" "X9"
-  sheet |> get_cell "A1" |> should equal "X9"
+  ("A1", "X9") |> should_become "X9"
 
 [<Fact>]
 let it_should_format_numbers_like_numbers() =
-  let sheet = new_sheet |> put_cell "A1" "7"
-  sheet |> get_cell "A1" |> should equal "7"
+  ("A1", "7") |> should_become "7"
+
 [<Fact>]
 let is_preserve_whitespace_in_strings() =
-  let sheet = new_sheet |> put_cell "A1" " X 99 "
-  sheet |> get_cell "A1" |> should equal " X 99 "
+  ("A1", " X 99 ") |> should_become " X 99 "
 
   
