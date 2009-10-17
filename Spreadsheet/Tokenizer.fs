1ca3bd54cddadbad9e775ef4631ed253ca6b7b0a
Ball <myotherpants@gmail.com>
2009-10-17 14:37:14 -0400

    Changed to active patterns

--- Spreadsheet/Tokenizer.fs
+++ Spreadsheet/Tokenizer.fs
@@ -3,21 +3,22 @@
 let tokenize (source:string) =
   let symbols = Set.of_list ['('; ')'; '+'; '-'; '/'; '*'] 
   let whiteSpaces = Set.of_list [' '; '\t'; '\n'; '\r']
-  let isSymbol (chr:char) = Set.mem chr symbols
-  let isWhiteSpace (chr:char) = Set.mem chr whiteSpaces
+  let (|Symbol|_|) chr = if Set.mem chr symbols then Some(chr) else None
+  let (|WhiteSpace|_|) chr = if Set.mem chr whiteSpaces then Some(chr) else None
+  let (|SymOrWs|_|) chr = if (Set.mem chr symbols) or (Set.mem chr whiteSpaces) then Some(chr) else None
   let join digits =
     let buffer = new System.Text.StringBuilder()
     List.rev digits |> List.map (fun (x:char) -> buffer.Append(x)) |> ignore
     buffer.ToString()
   let rec gather source digits tokens =
     match source with
-    | h::t when (isSymbol h) or (isWhiteSpace h) -> (join digits)::tokens |> loop source
-    | [] -> (join digits)::tokens |> loop source
+    | SymOrWs(h)::t -> loop source ((join digits)::tokens)
+    | [] -> loop source ((join digits)::tokens)
     | h::t -> gather t (h::digits) tokens  
   and loop source tokens =
     match source with
     | [] -> List.rev tokens
-    | h::t when isSymbol h -> loop t (h.ToString()::tokens)
-    | h::t when isWhiteSpace h -> loop t tokens
+    | Symbol(h)::t -> loop t (h.ToString()::tokens)
+    | WhiteSpace(h)::t -> loop t tokens
     | _ -> gather source [] tokens
   loop (source.ToCharArray() |> List.of_array) []
