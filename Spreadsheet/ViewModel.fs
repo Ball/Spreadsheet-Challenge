58486cf3b71205f2e11b3c130b43a2416e29721c
Brian J Ball <myotherpants@gmail.com>
2009-11-23 23:02:55 -0500

    Started the view models for the application

---
+++ Spreadsheet/ViewModel.fs
@@ -0,0 +1,23 @@
+﻿#light
+
+open Spreadsheet
+
+type CellViewModel(model:Map<string,string>, row:int) =
+  class
+    member x.InstanceIndexer
+      with get(column) = Spreadsheet.get_cell (column+row.ToString()) model
+  end
+type RawCellViewModel(model:Map<string,string>, row:int) =
+  class
+    member x.InstanceIndexer
+      with get(column) = Spreadsheet.get_literal (column+row.ToString()) model
+  end
+type RowViewModel(model:Map<string,string>, row:int) =
+  class
+    member x.Raw = new RawCellViewModel(model, row)
+    member x.Evaluated = new CellViewModel(model, row)
+  end
+
+let row_models row_count sheet =
+  [1 .. row_count]
+  |> List.map (fun x -> new RowViewModel(sheet, x))
---
+++ TestCases/Challenge4.fs
@@ -0,0 +1,42 @@
+﻿#light
+
+open Xunit
+open TestHelpers
+open Spreadsheet
+open FsxUnit.Syntax
+open ViewModel
+
+[<Fact>]
+let creates_row_models() =
+  sheet_with [("A1", "4"); ("A2", "=A1"); ("C4", "3")] new_sheet
+  |> row_models 4
+  |> List.length
+  |> should equal 4
+[<Fact>]
+let accesses_the_row() =
+  sheet_with [("A1", "4"); ("A2", "=A1"); ("C4", "3")] new_sheet
+  |> row_models 4
+  |> (fun x -> List.nth x 0)
+  |> (fun x -> x.Evaluated.InstanceIndexer("A"))
+  |> should equal "4"
+[<Fact>]
+let access_the_raw() =
+  sheet_with [("A1", "4"); ("A2", "=A1"); ("C4", "3")] new_sheet
+  |> row_models 4
+  |> (fun x -> List.nth x 1)
+  |> (fun x -> x.Raw.InstanceIndexer("A"))
+  |> should equal "=A1"
+[<Fact>]
+let access_the_evaluated() =
+  sheet_with [("A1", "4"); ("A2", "=A1"); ("C4", "3")] new_sheet
+  |> row_models 4
+  |> (fun x -> List.nth x 1)
+  |> (fun x -> x.Evaluated.InstanceIndexer("A"))
+  |> should equal "4"
+[<Fact>]
+let access_the_row_part_two() =
+  sheet_with [("A1", "4"); ("A2", "=A1"); ("C4", "3")] new_sheet
+  |> row_models 4
+  |> (fun x -> List.nth x 3)
+  |> (fun x -> x.Evaluated.InstanceIndexer("C"))
+  |> should equal "3"
